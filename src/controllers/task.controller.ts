36d2fa5da0ddbc83c06815769eb13fa05b6354fa
SergeySharipov <sharipov14@gmail.com>
2022-04-03 18:20:48 -0400

    Setup Task add/update validation

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,17 +1,45 @@
 import { Response, Request } from 'express'
 import { Task, User } from '../models/'
-import { ITaskCreation } from '../types/global'
+import zod from 'zod'
+
+const updateTaskValidation = zod.object({
+  id: zod.number({
+    required_error: 'id can\'t be empty',
+    invalid_type_error: 'id must be a number'
+  }),
+  userId: zod.number({
+    required_error: 'userId can\'t be empty',
+    invalid_type_error: 'userId must be a number'
+  }),
+  categoryId: zod.number({
+    required_error: 'categoryId can\'t be empty',
+    invalid_type_error: 'categoryId must be a number'
+  }),
+  title: zod.string({
+    required_error: 'title can\'t be empty',
+    invalid_type_error: 'title must be a string'
+  }).min(1, { message: 'title can\'t be empty' }),
+  description: zod.string({
+    invalid_type_error: 'description must be a string'
+  }).optional(),
+  status: zod.boolean({
+    invalid_type_error: 'status must be a boolean'
+  }).optional()
+})
+
+const addTaskValidation = updateTaskValidation.omit({ id: true })
 
 const addTask = async (req: Request, res: Response): Promise<void> => {
   try {
-    const task = req.body as ITaskCreation
-
     const user = await User.findByPk(req.userId)
 
     if (user) {
-      task.userId = user.id
+      const validTask = addTaskValidation.parse({
+        ...req.body,
+        userId: user.id
+      })
 
-      const newTask = await Task.create(task)
+      const newTask = await Task.create(validTask)
 
       res.status(201).json({
         message: 'Task added.',
@@ -20,19 +48,30 @@ const addTask = async (req: Request, res: Response): Promise<void> => {
     } else {
       res.status(500).send({ message: 'User not found.' })
     }
-  } catch (error) {
-    res.status(500).send({ message: 'Error: ' + error })
+  } catch (err: unknown) {
+    if (err instanceof zod.ZodError) {
+      res.status(500).send({ message: 'Error: ' + err.errors.map(m => m.message) })
+    } else {
+      res.status(500).send({ message: 'Error: ' + err })
+    }
   }
 }
 
 const updateTask = async (req: Request, res: Response): Promise<void> => {
   try {
-    const task = req.body as ITaskCreation
-
     const user = await User.findByPk(req.userId)
+    const taskId = Number(req.params.id)
 
     if (user) {
-      const updatedTask = await Task.update(task, { where: { id: req.params.id, userId: user.id } })
+      const validTask = updateTaskValidation.parse({
+        ...req.body,
+        userId: user.id,
+        id: taskId
+      })
+
+      const updatedTask = await Task.update(validTask, {
+        where: { id: validTask.id, userId: validTask.userId }
+      })
 
       res.status(201).json({
         message: 'Task updated.',
@@ -41,19 +80,24 @@ const updateTask = async (req: Request, res: Response): Promise<void> => {
     } else {
       res.status(500).send({ message: 'User not found.' })
     }
-  } catch (error) {
-    res.status(500).send({ message: 'Error: ' + error })
+  } catch (err: unknown) {
+    if (err instanceof zod.ZodError) {
+      res.status(500).send({ message: 'Error: ' + err.errors.map(m => m.message) })
+    } else {
+      res.status(500).send({ message: 'Error: ' + err })
+    }
   }
 }
 
 const deleteTask = async (req: Request, res: Response): Promise<void> => {
   try {
     const user = await User.findByPk(req.userId)
+    const taskId = Number(req.params.id)
 
     if (user) {
       const numberOfDeletedTasks = await Task.destroy({
         where: {
-          id: req.params.id,
+          id: taskId,
           userId: user.id
         }
       })
@@ -73,6 +117,7 @@ const deleteTask = async (req: Request, res: Response): Promise<void> => {
 const getTasks = async (req: Request, res: Response): Promise<void> => {
   try {
     const user = await User.findByPk(req.userId)
+
     if (user) {
       const tasks = await Task.findAll({
         where: {
