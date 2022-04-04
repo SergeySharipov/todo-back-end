e82c642a75293d53131b9c139c2a665622f86f56
SergeySharipov <sharipov14@gmail.com>
2022-04-03 20:18:39 -0400

    Refactor log.ts code

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,8 +5,11 @@ const DATABASE_URL = process.env.DATABASE_URL!
 
 const AUTH_SECRET = process.env.AUTH_SECRET!
 
+const NODE_ENV = process.env.NODE_ENV
+
 export {
   AUTH_SECRET,
   DATABASE_URL,
-  PORT
+  PORT,
+  NODE_ENV
 }
--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,11 +1,13 @@
+import { NODE_ENV } from './config'
+
 const i = (...params: unknown[]) => {
-  if (process.env.NODE_ENV === 'development') {
+  if (NODE_ENV === 'development') {
     console.log(...params)
   }
 }
 
 const e = (...params: unknown[]) => {
-  if (process.env.NODE_ENV === 'development') {
+  if (NODE_ENV === 'development') {
     console.error(...params)
   }
 }
